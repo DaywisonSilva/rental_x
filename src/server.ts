24e2bf7e3c2c02262bbd440e94279cf48fb916e0
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-06-18 18:27:24 -0300

    create category

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import categoriesRoutes from './routes/category.routes'
 
 const app = express()
 
+app.use(express.json())
+app.use(categoriesRoutes)
+
 app.listen(3000, () => console.log('server is running on port 3000'))
 
-app.use(express.json())
+app.get('/', (_, res) => {
+    return res.send({ message: 'Hello World' })
+})
